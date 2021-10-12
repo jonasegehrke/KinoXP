b3087b35d42428332e4f16de6f9eb0f62cb3aafb
jonasegehrke <jonasegehrke@gmail.com>
2021-10-12 13:12:45 +0200

    Removed backend

--- KinoBackend/src/main/java/com/example/kinobackend/repos/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.kinobackend.repos;
-
-import com.example.kinobackend.model.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovieRepository extends JpaRepository<Movie,Integer> {
-}
