2495284eb4c4487df82cb0edb955da7b0da474a0
AndreasGargulak <andr6230@stud.kea.dk>
2021-10-12 09:54:48 +0200

    Created Movie repository and movie model, aswell as REST controller

---
+++ KinoBackend/src/main/java/com/example/kinobackend/RESTController/MovieRESTController.java
@@ -0,0 +1,31 @@
+package com.example.kinobackend.RESTController;
+
+import com.example.kinobackend.model.Movie;
+import com.example.kinobackend.repos.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class MovieRESTController {
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    @GetMapping("/movies")
+    public List<Movie> getAllCurrentMovies(){
+        return movieRepository.findAll();
+    }
+
+    @PostMapping(value = "/movie", consumes = "application/json")
+    public ResponseEntity<Movie> addMovie(@RequestBody Movie movie){
+        movieRepository.save(movie);
+        return new ResponseEntity<Movie>(Movie, HttpStatus.CREATED);
+    }
+
+
+}
