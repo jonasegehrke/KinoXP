119feb9e44796744959b83b9e13df3f70588e75c
jonasegehrke <jonasegehrke@gmail.com>
2021-10-13 11:46:07 +0200

    changed to local rest instead of azure

--- KinoFrontend/js/movie.js
+++ KinoFrontend/js/movie.js
@@ -6,7 +6,8 @@ const durationInput = document.querySelector(".duration-input");
 const newMovieBtn = document.querySelector(".new-movie-btn");
 
 
-const movieUrl = `https://kinoxp.azurewebsites.net`;
+//const movieUrl = `https://kinoxp.azurewebsites.net`;
+const movieUrl = `http://localhost:8080`;
 
 async function getMovies() {
     const resp = await fetch(movieUrl + "/movies");
