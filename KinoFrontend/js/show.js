0a44d6fb11213d9347f193c10df9ff86a37d912a
Jimmi Pisalita <mig.jimmi@gmail.com>
2021-10-14 10:59:40 +0200

    Unfinished

--- KinoFrontend/js/show.js
+++ KinoFrontend/js/show.js
@@ -2,12 +2,8 @@ const dateInput = document.querySelector(".date-input");
 const timeInput = document.querySelector(".time-input");
 const theaterInput = document.querySelector(".theater-input");
 const dropDownMovies = document.querySelector(".drop-down-movies");
-const dropDownTheaters = document.querySelector(".theater-drop-down");
 const newShowBtn = document.querySelector(".new-show-btn");
 const showResult = document.querySelector(".show-result");
-const showInputFields = document.querySelectorAll(".form-control");
-const formSelects = document.querySelectorAll(".form-select");
-let theater = null;
 
 //const url = `https://kinoxp.azurewebsites.net`;
 const url = `http://localhost:8080`;
@@ -33,32 +29,16 @@ async function newShow(data) {
     })
 }
 
-async function newTheater(data) {
-    let theaterTemp = null;
-    await fetch(url + "/theater", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: { "Content-type": "application/json; charset=UTF-8" }
-    }).then((response) => response.json())
-    .then((data) => {
-        theaterTemp = data;
-    }).then(() => console.log(theaterTemp))
-
-    theater = theaterTemp;
-}
-
 function showTableHeadlines() {
     let rowCount = showResult.rows.length;
     let row = showResult.insertRow(rowCount);
 
-    row.insertCell(0).innerHTML = `ShowId`;
-    row.insertCell(1).innerHTML = `Date`;
-    row.insertCell(2).innerHTML = `Time`;
-    row.insertCell(3).innerHTML = `Theater`;
-    row.insertCell(4).innerHTML = `Movie`;
-    row.insertCell(5).innerHTML = `Duration`;
-    row.insertCell(6).innerHTML = `Book`;
-    row.insertCell(7).innerHTML = 'Delete <i class="uil uil-trash-alt"></i>';
+    row.insertCell(0).innerHTML = `showId`;
+    row.insertCell(1).innerHTML = `date`;
+    row.insertCell(2).innerHTML = `time`;
+    row.insertCell(3).innerHTML = `theater`;
+    row.insertCell(4).innerHTML = `movie`;
+    row.insertCell(5).innerHTML = 'Delete <i class="uil uil-trash-alt"></i>';
     row.setAttribute("id", "table-headline");
 }
 
@@ -77,16 +57,10 @@ function addRow(respData) {
         row.insertCell(0).innerHTML = show.showId;
         row.insertCell(1).innerHTML = show.date;
         row.insertCell(2).innerHTML = show.time;
-        row.insertCell(3).innerHTML = show.theater.name;
-        row.insertCell(4).innerHTML = show.movie.title;
-        row.insertCell(5).innerHTML = show.movie.movieDuration + " min";
-        row.insertCell(6).innerHTML = `<a onclick="redirectToBooking(${show.showId})"> <button type="button" class="btn btn-secondary">Book Show</button></a>`;
-        row.insertCell(7).innerHTML = `<a onclick="deleteRow(this)"> <button type="button" class="btn btn-secondary uil uil-trash-alt"></button></a>`;
+        row.insertCell(3).innerHTML = show.theater;
+        row.insertCell(4).innerHTML = show.movie;
+        row.insertCell(5).innerHTML = `<a onclick="deleteRow(this)"> <button class="delete-show-btn uil uil-trash-alt"></button></a>`;
     }
-}
-
-function redirectToBooking(id){
-    location.replace('/html/show-booking.html?showId=' + id);
 }
 
 function deleteRow(rowObj) {
@@ -99,45 +73,34 @@ function deleteRow(rowObj) {
 if (newShowBtn) {
     newShowBtn.addEventListener("click", (e) => {
         e.preventDefault();
-        console.log(dropDownMovies.value);
-        const movie = JSON.parse(dropDownMovies.value);
         let data = {
             date: dateInput.value,
             time: timeInput.value,
-            theater: theater,
-            movie: movie
+            theater: theaterInput.value,
+            movie: dropDownMovies.value
         }
         console.log(data);
         if (data) {
-            console.log(data, " sent to REST")
+            console.log(data + " sent to REST")
             newShow(data)
-
-            for(let i = 0; i < showInputFields.length; i++){
-                showInputFields[i].value = '';
-            }
-            for(let i = 0; i < formSelects.length; i++){
-                formSelects[i].selectedIndex = 0;
-            }
-
-            alert("Show Created!")
         }
     })
 }
 
-
-function fillDropDownMovies(movie, theater) {
+async function fillDropDownMovies(movie) {
         const el = document.createElement("option");
         el.textContent = movie.title;
         el.setAttribute("value", `{
-            "id":"${movie.id}",
-            "title":"${movie.title}",
-            "genre":"${movie.genre}",
-            "ageLimit":"${movie.ageLimit}",
-            "movieDuration":"${movie.movieDuration}",
-            "artist":"${movie.artist}"
-            }`);
+            "id":${movie.id}, 
+            "title":${movie.title},
+            "genre":${movie.genre},
+            "ageLimit":${movie.ageLimit},
+            "movieDuration":${movie.movieDuration},
+            "artist":${movie.artist}
+        }`);
         dropDownMovies.appendChild(el);
     }
+
 
 async function getMoviesForDropDown() {
     data = await fetch(url + "/movies");
@@ -147,6 +110,7 @@ async function getMoviesForDropDown() {
 
 getMoviesForDropDown();
 
-dropDownTheaters.addEventListener("change", async function(){
-    await newTheater(JSON.parse(dropDownTheaters.value));
+dropDownMovies.addEventListener('change', function (){
+    let value = JSON.parse(JSON.stringify(dropDownMovies.value));
+    console.log(value.id);
 })
