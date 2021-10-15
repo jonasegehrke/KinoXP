6bbf630d9fa5a71da7c5f6998568706485c03a59
jonasegehrke <jonasegehrke@gmail.com>
2021-10-15 12:51:55 +0200

    deleted backend

--- KinoBackend/src/main/java/com/example/kinobackend/repos/ShowRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.kinobackend.repos;
-
-import com.example.kinobackend.model.Show;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShowRepository extends JpaRepository<Show, Integer> {
-}
