c4c3476f70c522ebe8ec6c52f7672bb14a9f29ba
jonasegehrke <jonasegehrke@gmail.com>
2021-10-18 12:13:56 +0200

    delete and edit now works

--- KinoFrontend/js/booking-list.js
+++ KinoFrontend/js/booking-list.js
@@ -23,12 +23,12 @@ function bookingTableHeadlines() {
 
     row.insertCell(0).innerHTML = `Booking Id`;
     row.insertCell(1).innerHTML = `Booking Number`;
-    row.insertCell(2).innerHTML = `Number of seats`;
+    row.insertCell(2).innerHTML = `Number of seats (Editable)`;
     row.insertCell(3).innerHTML = `Show Id`;
     row.insertCell(4).innerHTML = `Show Title`;
     row.insertCell(5).innerHTML = `Show Date`;
     row.insertCell(6).innerHTML = `Show Time`;
-    row.insertCell(7).innerHTML = `Edit Booking`;
+    row.insertCell(7).innerHTML = `Save Edit`;
     row.insertCell(8).innerHTML = 'Delete <i class="uil uil-trash-alt"></i>';
 
     row.setAttribute("id", "table-headline");
@@ -47,18 +47,18 @@ function addRow(respData) {
             showTime: respData[i].show.time
 
         }
-        console.log(booking)
+        
         let rowCount = bookingResult.rows.length;
         let row = bookingResult.insertRow(rowCount);
 
         row.insertCell(0).innerHTML = booking.bookingId;
         row.insertCell(1).innerHTML = booking.bookingNumber;
-        row.insertCell(2).innerHTML = booking.numberOfSeats;
+        row.insertCell(2).innerHTML = `<p contentEditable="true">${booking.numberOfSeats}</p>`;
         row.insertCell(3).innerHTML = booking.show.showId;
         row.insertCell(4).innerHTML = booking.show.movie.title;
         row.insertCell(5).innerHTML = booking.show.date;
         row.insertCell(6).innerHTML = booking.show.time;
-        row.insertCell(7).innerHTML = "INSERT EDIT BOOKING HERE"
+        row.insertCell(7).innerHTML = `<a onclick="saveRow(this)"><button type="button" class="btn btn-secondary uil uil-save"></button></a>`
         row.insertCell(8).innerHTML = `<a onclick="deleteRow(this)"> <button type="button" class="btn btn-secondary uil uil-trash-alt"></button></a>`;
     }
 }
@@ -66,8 +66,94 @@ function addRow(respData) {
 function deleteRow(rowObj) {
     let row = rowObj.parentNode.parentNode;
     let table = row.parentNode;
+
+    const numberOfSeats = row.childNodes[2].firstChild.textContent;
+    const showId = row.childNodes[3].firstChild.nodeValue;
+    removeSeatsFromTheater(showId, numberOfSeats);
+
     deleteBooking(row.childNodes[0].firstChild.nodeValue);
     table.removeChild(row);
+}
+
+function saveRow(rowObj) {
+    let row = rowObj.parentNode.parentNode;
+    let table = row.parentNode;
+
+    const numberOfSeats = row.childNodes[2].firstChild.textContent;
+    const showId = row.childNodes[3].firstChild.nodeValue;
+    const bookingId = row.childNodes[0].firstChild.nodeValue;
+    //removeSeatsFromTheater(showId, numberOfSeats);
+ 
+    updateSeatsFromTheater(bookingId, numberOfSeats);
+    //deleteBooking(row.childNodes[0].firstChild.nodeValue);
+    //table.removeChild(row);
+}
+
+async function updateSeatsFromTheater(bookingId, numberOfSeats){
+    const resp = await fetch(url + "/booking/inspect/" + bookingId);
+    const respData = await resp.json();
+
+   
+    let oldSeats = respData.numberOfSeats;
+    let availableSeats = respData.show.theater.availableSeats;
+    
+    if(numberOfSeats > oldSeats){
+        availableSeats = availableSeats - (numberOfSeats - oldSeats)
+    }else if(numberOfSeats < oldSeats){
+        availableSeats = availableSeats + (oldSeats - numberOfSeats)
+    }
+
+    let theater = {
+        theaterId: respData.show.theater.theaterId,
+        name: respData.show.theater.name,
+        big: respData.show.theater.big,
+        availableSeats: availableSeats
+    }
+
+    let booking = {
+        bookingId: respData.bookingId,
+        bookingNumber: respData.bookingNumber,
+        numberOfSeats: numberOfSeats,
+        show: respData.show
+    }
+
+    await fetch(url + "/booking/update", {
+        method: "PUT",
+        body: JSON.stringify(booking),
+        headers: { "Content-type": "application/json; charset=UTF-8" }
+    })
+
+    await fetch(url + "/theater/update", {
+        method: "PUT",
+        body: JSON.stringify(theater),
+        headers: { "Content-type": "application/json; charset=UTF-8" }
+    })
+
+
+}
+
+async function removeSeatsFromTheater(showId, numberOfSeats){
+    const resp = await fetch(url + "/show/inspect/" + showId);
+    const respData = await resp.json();
+
+
+
+    let availableSeats = parseInt(respData.theater.availableSeats);
+    numberOfSeats = parseInt(numberOfSeats);
+    availableSeats = availableSeats + numberOfSeats;
+
+    let theater = {
+        theaterId: respData.theater.theaterId,
+        name: respData.theater.name,
+        big: respData.theater.big,
+        availableSeats: availableSeats
+    }
+
+    await fetch(url + "/theater/update", {
+        method: "PUT",
+        body: JSON.stringify(theater),
+        headers: { "Content-type": "application/json; charset=UTF-8" }
+    })
 }
 
 getBookings();
