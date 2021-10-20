c8f602da9bad483299a9fb3f83ac7fd93a887a4d
jonasegehrke <jonasegehrke@gmail.com>
2021-10-20 13:01:56 +0200

    removed backend ;)

--- KinoBackend/src/main/java/com/example/kinobackend/repos/KioskItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.kinobackend.repos;
-
-import com.example.kinobackend.model.KioskItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KioskItemRepository extends JpaRepository<KioskItem, Integer> {
-}
--- KinoBackend/src/main/java/com/example/kinobackend/repos/TheaterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.kinobackend.repos;
-
-import com.example.kinobackend.model.Theater;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TheaterRepository extends JpaRepository<Theater, Integer> {
-}
